6c144b412f488e9dcd61fdad155e2c6a1045c301
leviettinh1479 <115515237+leviettinh1479@users.noreply.github.com>
2023-07-15 11:00:15 +0700

    Move_1

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+    
+
+    private Rigidbody2D rigidbody2D;
+    private Animator ani;
+    private SpriteRenderer spriteRenderer;
+    private float dirX = 0f;
+    [SerializeField]
+    private float movespeed = 7f;
+    [SerializeField]
+    public bool nendat;
+    private float jumpF = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+       
+
+        //particleSystem.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        dirX = Input.GetAxisRaw("Horizontal");
+        rigidbody2D.velocity = new Vector2(dirX * movespeed, rigidbody2D.velocity.y);
+        //material.SetFloat("_Fade", 0.1f);
+        if (Input.GetButtonDown("Jump"))
+        {
+          
+            if (nendat == true)
+            {
+                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpF);
+
+                nendat = false;
+            }
+
+
+        }
+       
+        UpdateAni();
+    }
+    private void UpdateAni()
+    {
+        if (dirX > 0f)
+        {
+            ani.SetBool("IsRunning", true);
+            spriteRenderer.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            ani.SetBool("IsRunning", true);
+            spriteRenderer.flipX = true;
+
+
+        }
+        else
+        {
+            ani.SetBool("IsRunning", false);
+
+
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "nendat")
+        {
+
+            nendat = true;
+        }
+
+
+
+    }
+
+}
+
